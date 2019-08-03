5f3f4bc38c69c69407fca0c58e19193c7bb3f661
Cong Xu <congusbongus@gmail.com>
2019-08-03 14:54:28 +1000

    Filter suburbs by city bounds

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # where-to-live
 Python script for finding an ideal suburb to live in
+
+## Data
+Australian post codes from http://www.corra.com.au/australian-postcode-location-data/
+
+BSD licence
+
+Warning: out of date
+
+TODO: pay for https://www.australiantownslist.com?
--- test.py
+++ test.py
@@ -1,15 +1,33 @@
 import os
 
 import googlemaps
+import pandas as pd
 from datetime import datetime
+from munch import munchify
+
+
+def load_au_suburbs() -> pd.DataFrame:
+    return pd.read_csv("Australian_Post_Codes_Lat_Lon.csv")
+
+
+def find_locales_inside(gmaps: googlemaps.Client, au_suburbs: pd.DataFrame, address: str):
+    target = munchify(gmaps.geocode(address))
+    bounds = target[0].geometry.bounds
+    return au_suburbs.loc[
+        (au_suburbs['lat'] >= bounds.southwest.lat) &
+        (au_suburbs['lat'] <= bounds.northeast.lat) &
+        (au_suburbs['lon'] >= bounds.southwest.lng) &
+        (au_suburbs['lon'] <= bounds.northeast.lng)
+    ]
 
 
 def main(gm_key: str):
     gmaps = googlemaps.Client(key=gm_key)
+    au_suburbs = load_au_suburbs()
 
-    # Geocoding an address
-    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-    print(geocode_result)
+    # Find suburbs in city
+    suburbs = find_locales_inside(gmaps, au_suburbs, "Sydney NSW Australia")
+    print(suburbs)
 
     # Look up an address with reverse geocoding
     reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
